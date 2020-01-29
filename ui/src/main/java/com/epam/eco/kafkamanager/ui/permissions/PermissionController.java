b2ad9ec7b4ad1465ae49c4319adbdf16de2821e7
Raman_Babich <Raman_Babich@epam.com>
2020-01-29 16:09:04 +0300

    Expose draft version of the ace add/remove functionality on the ui
    It looks like we need to rework metadata associated with permissions and
    also handle not only PatternType.LITERAL permissions, that will possibly
    affect the KafkaManager interface.
    
    Relates to #6.

--- ui/src/main/java/com/epam/eco/kafkamanager/ui/permissions/PermissionController.java
+++ ui/src/main/java/com/epam/eco/kafkamanager/ui/permissions/PermissionController.java
@@ -22,19 +22,29 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
+import org.apache.kafka.common.resource.ResourceType;
+import org.apache.kafka.common.security.auth.KafkaPrincipal;
+import org.apache.kafka.common.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.epam.eco.kafkamanager.KafkaManager;
+import com.epam.eco.kafkamanager.Metadata;
+import com.epam.eco.kafkamanager.PermissionCreateParams;
+import com.epam.eco.kafkamanager.PermissionDeleteParams;
 import com.epam.eco.kafkamanager.PermissionInfo;
 import com.epam.eco.kafkamanager.PermissionSearchQuery;
 import com.epam.eco.kafkamanager.ui.permissions.export.PermissionExporterType;
@@ -47,20 +57,28 @@ public class PermissionController {
 
     private static final int PAGE_SIZE = 10;
 
+    public static final String PERMISSION_CREATE_VIEW = "permission_create";
+
     public static final String VIEW = "permissions";
     public static final String ATTR_PAGE = "page";
     public static final String ATTR_SEARCH_QUERY = "searchQuery";
     public static final String ATTR_TOTAL_COUNT = "totalCount";
+    public static final String ATTR_DEFAULT_RESOURCE_TYPE = "defaultResourceType";
+    public static final String ATTR_DEFAULT_OPERATION = "defaultOperation";
+    public static final String ATTR_DEFAULT_PERMISSION_TYPE = "defaultPermissionType";
 
     public static final String MAPPING = "/permissions";
+    public static final String MAPPING_PERMISSION = MAPPING + "/{resourceType}/{resourceName}/{permissionType}/{operation}/{principal}/{host}";
     public static final String MAPPING_EXPORT = MAPPING + "/export";
+    public static final String MAPPING_CREATE = "/permission_create";
+    public static final String MAPPING_DELETE = MAPPING_PERMISSION + "/delete";
 
     @Autowired
     private KafkaManager kafkaManager;
 
-    @RequestMapping(value=MAPPING, method=RequestMethod.GET)
+    @RequestMapping(value = MAPPING, method = RequestMethod.GET) // TODO: what about not PatternType.LITERAL permissions?
     public String permissions(
-            @RequestParam(required=false) Integer page,
+            @RequestParam(required = false) Integer page,
             @RequestParam Map<String, Object> paramsMap,
             Model model) {
         PermissionSearchQuery searchQuery = PermissionSearchQuery.fromJson(paramsMap);
@@ -77,7 +95,7 @@ public class PermissionController {
         return VIEW;
     }
 
-    @RequestMapping(value=MAPPING_EXPORT, method=RequestMethod.GET)
+    @RequestMapping(value = MAPPING_EXPORT, method = RequestMethod.GET)
     public void export(
             @RequestParam PermissionExporterType exporterType,
             @RequestParam Map<String, Object> paramsMap,
@@ -98,6 +116,67 @@ public class PermissionController {
             exporterType.exporter().export(permissionInfos, out);
             out.flush();
         }
+    }
+
+    @RequestMapping(value = MAPPING_CREATE, method = RequestMethod.GET)
+    public String create(Model model) {
+        model.addAttribute(ATTR_DEFAULT_RESOURCE_TYPE, ResourceType.TOPIC);
+        model.addAttribute(ATTR_DEFAULT_PERMISSION_TYPE, AclPermissionType.ALLOW);
+        model.addAttribute(ATTR_DEFAULT_OPERATION, AclOperation.DESCRIBE);
+
+        return PERMISSION_CREATE_VIEW;
+    }
+
+    @RequestMapping(value = MAPPING_CREATE, method = RequestMethod.POST) // TODO: permissions metadata crud is not so intuitive.
+    public String create(
+            @RequestParam ResourceType resourceType,
+            @RequestParam String resourceName,
+            @RequestParam AclPermissionType permissionType,
+            @RequestParam AclOperation operation,
+            @RequestParam String principal,
+            @RequestParam String host) {
+        KafkaPrincipal kafkaPrincipal = SecurityUtils.parseKafkaPrincipal(principal);
+        PermissionCreateParams.Builder createParamsBuilder = PermissionCreateParams.builder()
+                .resourceType(resourceType)
+                .resourceName(resourceName)
+                .permissionType(permissionType)
+                .operation(operation)
+                .principal(kafkaPrincipal)
+                .host(host);
+        PermissionSearchQuery query = PermissionSearchQuery.builder()
+                .resourceType(resourceType)
+                .resourceName(resourceName)
+                .kafkaPrincipal(principal)
+                .build();
+        Page<PermissionInfo> permissionPage = kafkaManager.getPermissionPage(query, PageRequest.of(0, 1)); // TODO: silly workaround, it is not correct.
+        if (!permissionPage.isEmpty()) {
+            Optional<Metadata> metadata = permissionPage.iterator().next().getMetadata();
+            metadata.ifPresent(value ->
+                    createParamsBuilder.description(value.getDescription()).attributes(value.getAttributes()));
+        }
+        kafkaManager.createPermission(createParamsBuilder.build());
+        return "redirect:" + ResourcePermissionController.buildResourcePermissionUrl(
+                resourceType, resourceName, kafkaPrincipal);
+    }
+
+    @RequestMapping(value = MAPPING_DELETE, method = RequestMethod.POST)
+    public String delete(
+            @PathVariable("resourceType") ResourceType resourceType,
+            @PathVariable("resourceName") String resourceName,
+            @PathVariable("permissionType") AclPermissionType permissionType,
+            @PathVariable("operation") AclOperation operation,
+            @PathVariable("principal") String principal,
+            @PathVariable("host") String host) { // TODO: it also deletes metadata that is not correct in many cases.
+        PermissionDeleteParams deleteParams = PermissionDeleteParams.builder()
+                .resourceType(resourceType)
+                .resourceName(resourceName)
+                .permissionType(permissionType)
+                .operation(operation)
+                .principal(principal)
+                .host(host)
+                .build();
+        kafkaManager.deletePermission(deleteParams);
+        return "redirect:" + MAPPING;
     }
 
     private Page<PermissionInfoWrapper> wrap(Page<PermissionInfo> page) {
