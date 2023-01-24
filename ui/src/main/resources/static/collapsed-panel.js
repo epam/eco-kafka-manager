2c72cab6b4e737010a74159db3d7bcd69fae80d8
michael200kg <michael.vershkov@gmail.com>
2023-01-24 16:57:05 +0600

    Fix styles for bootstrap 5 (#39)
    Co-authored-by: Mikhail_Vershkov <Mikhail_Vershkov@epam.com>

---
+++ ui/src/main/resources/static/collapsed-panel.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+$(document).ready(function() {
+
+    isLocalStorageSuports = function () {
+        return (typeof(Storage) !== "undefined");
+    }
+
+    getDestanationState = function (iconItem) {
+        if (iconItem.classList.contains('fa-caret-down')) {
+            return "opened-state";
+        } else {
+            return "closed-state";
+        }
+    };
+
+    changeCollapseButtonState = function (button, destinationState) {
+        let iconItem = button.children[0];
+        if (destinationState === 'opened-state') {
+            iconItem.classList.replace('fa-caret-down', 'fa-caret-up');
+        } else {
+            iconItem.classList.replace('fa-caret-up', 'fa-caret-down');
+        }
+        if(isLocalStorageSuports()) {
+            localStorage.setItem(button.id, destinationState);
+        }
+    };
+
+    document.querySelectorAll(".collapsed-header").forEach(item =>
+        item.addEventListener('click',
+            function (event) {
+                let button = event.currentTarget.querySelector(".collapse-button");
+                if(button) {
+                    button.click();
+                }
+            })
+    );
+
+    document.querySelectorAll(".collapse-button").forEach(item => item.addEventListener('click',
+        function (event) {
+            let button = event.currentTarget;
+            let destinationState = getDestanationState(button.children[0]);
+            changeCollapseButtonState(button, destinationState);
+        })
+    );
+
+    document.querySelectorAll(".collapse-button").forEach(button => {
+        if(isLocalStorageSuports()) {
+            var storedState = localStorage.getItem(button.id);
+            if (storedState !== null) {
+                changeCollapseButtonState(button, storedState);
+                let panel = document.querySelector(button.getAttribute("href"));
+                if (storedState === 'opened-state') {
+                    panel.classList.remove("show");
+                    panel.classList.add("show");
+                } else {
+                    panel.classList.remove("show");
+                }
+            }
+        }
+    });
+
+});
